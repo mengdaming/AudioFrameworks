7eca2df9f0149dcd7be5f40897f3d22a375ca72a
Damien Menanteau <damien.menanteau@gmail.com>
2019-11-12 00:07:34 +0100

    Tune taglib framework

---
+++ taglib/taglib/taglib.h
@@ -0,0 +1,13 @@
+//
+//  taglib.h
+//  taglib
+//
+//  Created by Damien Menanteau on 09/11/2019.
+//  Copyright © 2019 sbooth.org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface taglib : NSObject
+
+@end
---
+++ taglib/taglib/taglib.m
@@ -0,0 +1,13 @@
+//
+//  taglib.m
+//  taglib
+//
+//  Created by Damien Menanteau on 09/11/2019.
+//  Copyright © 2019 sbooth.org. All rights reserved.
+//
+
+#import "taglib.h"
+
+@implementation taglib
+
+@end
